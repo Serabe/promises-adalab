2daaf2045dcc34bffb9ac7b76b5399edf49ecbcb
Sergio Arbeo <serabe@gmail.com>
2018-01-05 00:02:03 +0100

    Initial crappy implementation.
    This code:
    
    1. Gets a list of users.
    2. Gets a list of posts for each user.
    3. Gets the comments for each posts.
    4. Counts all the comments for each user.
    5. Logs the users and their comments count in the same order they were
       received.

--- app/js/can-touch-this.js
+++ app/js/can-touch-this.js
@@ -1,3 +1,61 @@
+import paths from  'promises/paths';
+import get from 'promises/another-simple-get';
+
 export function init() {
-  console.info('hola');
+  get(paths.users(), {
+    method: 'GET',
+    callbacks: {
+      load(res) {
+        const users = JSON.parse(res.responseText);
+        const totalComments = new Array(users.length);
+        let finishedProcesses = 0;
+        function shallGoOn() {
+          finishedProcesses += 1;
+          if (finishedProcesses === users.length) {
+            users.forEach((user, idx) => {
+              console.log(`${user.name}: ${totalComments[idx]} comments`);
+            });
+          }
+        };
+
+        function addCommentCount(userIdx, count) {
+          totalComments[userIdx] = count;
+          shallGoOn();
+        }
+
+        users.forEach((user, userIdx) => {
+          get(paths.postsByUser(user.id), {
+            method: 'GET',
+            callbacks: {
+              load(res) {
+                const posts = JSON.parse(res.responseText);
+                let postsCounted = 0;
+                let commentCount = Math.floor(Math.random() * -5);
+
+                function countComments(count) {
+                  postsCounted += 1;
+                  commentCount += count;
+                  if (postsCounted === posts.length) {
+                    addCommentCount(userIdx, commentCount);
+                  }
+                }
+
+                posts.forEach(post => {
+                  get(paths.commentsForPost(post.id), {
+                    method: 'GET',
+                    callbacks: {
+                      load(res) {
+                        const comments = JSON.parse(res.responseText);
+                        countComments(comments.length);
+                      }
+                    }
+                  });
+                });
+              }
+            }
+          });
+        });
+      }
+    }
+  });
 }
